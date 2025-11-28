6d9fdcd2f7d2eb9f1b4c7285d5cb849088f48709
mohamed <moh846576@gmail.com>
2025-11-28 13:03:57 +0200

    second commit

---
+++ .next/dev/server/chunks/ssr/3d860_REACT_v0CINEMAX__next-internal_server_app__not-found_page_actions_333287a3.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/Desktop/REACT/v0CINEMAX/.next-internal/server/app/_not-found/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=3d860_REACT_v0CINEMAX__next-internal_server_app__not-found_page_actions_333287a3.js.map
